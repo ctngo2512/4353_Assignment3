7c592c0639b40f78fd73c75ca3a460936c0c5c8c
Chi Ngo <70230102+ctngo2512@users.noreply.github.com>
2021-07-10 19:27:05 -0500

    bloop random testing

---
+++ client/src/Login.test.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Login from './Login';
+import App from './App';
+
+    describe('Login component tests', ()=> {
+        const wrapper = shallow(<Login />);
+
+        it('should have a btn component', ()=> {
+
+            //There should be only one button
+            expect(wrapper.find('button')).toHaveLength(1);
+
+            //Button should be of type button
+            expect(wrapper.find('Button')
+            .type().defaultProps.type)
+            .toEqual('button');
+
+            //Button should have matching text
+            expect(wrapper.find('Button').text()).toEqual('Sign In');
+        });
+
+        it('should have input for email and password', ()=> {
+            //Email and password input field should be present
+            expect(wrapper.find('input#email')).toHaveLength(1);
+            expect(wrapper.find('input#password')).toHaveLength(1);
+        });
+
+        it('should have an empty email and password state var', ()=> {
+            //Optionally test to check if password and email are empty strings on 
+              // setup
+            expect(wrapper.state('email')).toEqual('');
+            expect(wrapper.state('password')).toEqual('');
+        });
+
+    });
