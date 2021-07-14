18fc7e96167485d8477fdfdab23c02cef9716cdd
Chi Ngo <70230102+ctngo2512@users.noreply.github.com>
2021-07-14 13:34:18 -0500

    contactform test

--- client/src/contactForm.test.js
+++ client/src/contactForm.test.js
@@ -1,13 +1,17 @@
 import React from 'react';
+import Enzyme from 'enzyme';
 import {shallow, mount} from 'enzyme';
 import ContactForm from './contactForm';
 import {handleFormSubmit} from './contactForm';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+
+Enzyme.configure({ adapter: new Adapter() });
 
 describe('Contact Form component tests', ()=> {
 
-    it("renders without crashing", () => {
+    /*it("renders without crashing", () => {
         shallow(<ContactForm />);
-    });
+    });*/
 
     it ('calls onSubmit prop function when form is submitted', () => {
         const wrapper = mount(<ContactForm onSubmit={handleFormSubmit}/>);
