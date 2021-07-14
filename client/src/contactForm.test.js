70fd16a32a78c1a5b719ea84f1bccb9695cafad0
Chi Ngo <70230102+ctngo2512@users.noreply.github.com>
2021-07-14 13:22:01 -0500

    test for contactform

--- client/src/contactForm.test.js
+++ client/src/contactForm.test.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import {shallow, mount} from 'enzyme';
 import ContactForm from './contactForm';
+import {handleFormSubmit} from './contactForm';
 
 describe('Contact Form component tests', ()=> {
-    const wrapper = shallow(<ContactForm />);
 
     it("renders without crashing", () => {
         shallow(<ContactForm />);
     });
+
+    it ('calls onSubmit prop function when form is submitted', () => {
+        const wrapper = mount(<ContactForm onSubmit={handleFormSubmit}/>);
+        const form = wrapper.find('form');
+        form.simulate('submit');
+        expect(handleFormSubmit).toHaveBeenCalledTimes(1);
+    })
 });
