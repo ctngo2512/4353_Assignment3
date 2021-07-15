3ca6c84ba371bff2b0ea3d437f5f1750c87512bd
Chi Ngo <70230102+ctngo2512@users.noreply.github.com>
2021-07-15 18:14:35 -0500

    fixing tests

--- client/src/Hero.test.js
+++ client/src/Hero.test.js
@@ -4,39 +4,10 @@ import {shallow, mount} from 'enzyme';
 import Hero from './Hero';
 import {handleLogout} from './Hero';
 import {goBack} from './Hero';
-import { act, render } from '@testing-library/react';
-import ContactForm from './contactForm';
 
 describe('Profile component tests', ()=> {
     
     const wrapper = shallow(<Hero />);
-    
-    it('Renders user data', () => {
-        let fakeUser = {
-            name: 'Meep',
-            address: '1234 street',
-            city: 'houston',
-            state: 'TX',
-            zipcode: '12345'
-        }
-    
-        let snapshot = {val: () => fakeUser};
-        jest.spyOn(fire, 'database').mockImplementation(() => ({
-            ref: jest.fn().mockReturnThis(),
-            on: jest.fn((event, callback) => callback(snapshot))
-        }));
-    
-        act(() => {
-            render(<ContactForm/>);
-        });
-    
-        expect(container.querySelector('.Name').textContent).toBe(fakeUser.name);
-        expect(container.querySelector('.Address').textContent).toBe(fakeUser.address);
-        expect(container.querySelector('.City').textContent).toBe(fakeUser.city);
-        expect(container.querySelector('.State').textContent).toBe(fakeUser.state);
-        expect(container.querySelector('.Zipcode').textContent).toBe(fakeUser.zipcode);
-    
-    });
 
     it("renders without crashing", () => {
         shallow(<Hero />);
--- client/src/contactForm.test.js
+++ client/src/contactForm.test.js
@@ -2,11 +2,41 @@ import React from 'react';
 import {shallow, mount} from 'enzyme';
 import ContactForm from './contactForm';
 import {handleFormSubmit} from './contactForm';
+import { act, render } from '@testing-library/react';
+import fire from './fire';
 
 describe('Contact Form component tests', ()=> {
+    const wrapper = shallow(<ContactForm />);
 
     it("renders without crashing", () => {
         shallow(<ContactForm />);
+    });
+
+    it('Renders user data', () => {
+        let fakeUser = {
+            name: 'Meep',
+            address: '1234 street',
+            city: 'houston',
+            state: 'TX',
+            zipcode: '12345'
+        }
+    
+        let snapshot = {val: () => fakeUser};
+        jest.spyOn(fire, 'database').mockImplementation(() => ({
+            ref: jest.fn().mockReturnThis(),
+            on: jest.fn((event, callback) => callback(snapshot))
+        }));
+    
+        act(() => {
+            render(<ContactForm/>);
+        });
+    
+        expect(wrapper.find('.name').textContent).toBe(fakeUser.name);
+        expect(wrapper.find('.address').textContent).toBe(fakeUser.address);
+        expect(wrapper.find('.city').textContent).toBe(fakeUser.city);
+        expect(wrapper.find('.state').textContent).toBe(fakeUser.state);
+        expect(wrapper.find('.zipcode').textContent).toBe(fakeUser.zipcode);
+    
     });
 
     it ('calls onSubmit prop function when form is submitted', () => {
